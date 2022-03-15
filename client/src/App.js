3e91c84a325eab146e7a82dadf4f673fa990b334
tedtalksbits <tedaneblake@gmail.com>
2022-03-15 17:14:40 -0400

    :star: frontend: added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Admin } from "./pages/Admin";
+import { Post } from "./pages/Post";
+import { Home } from "./pages/Home";
+import Nav from "./components/Nav";
 
 function App() {
-   return <div className="app">hello world</div>;
+   return (
+      <div className="app">
+         <Router>
+            <Nav />
+            <Routes>
+               <Route path="/" exact element={<Home />} />
+               <Route path="/admin" element={<Admin />} />
+               <Route path="/post" element={<Post />} />
+            </Routes>
+         </Router>
+      </div>
+   );
 }
 
 export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Home = () => {
+   return (
+      <div>
+         <h1>Welcome to the PrePrint ™️ </h1>
+      </div>
+   );
+};
