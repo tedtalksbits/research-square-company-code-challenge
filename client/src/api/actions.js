65b49eb63bf23ed1ec0ed707733583b4072699be
tedtalksbits <tedaneblake@gmail.com>
2022-03-23 11:22:10 -0400

    chore: code splitting: separate api actions

--- client/src/api/actions.js
+++ client/src/api/actions.js
@@ -1,11 +1,44 @@
 // get all approved articles
-const FETCH_APPROVED = 'http://localhost:5099/api/articles';
+import { API } from './contants';
 
-export const getApproved = async () => {
+export const getArticles = async () => {
   try {
-    const res = await fetch(FETCH_APPROVED);
+    const res = await fetch(API);
     const data = await res.json();
     return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// post new article
+export const postArticle = async newArticle => {
+  try {
+    const res = await fetch(API, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newArticle),
+    });
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// put new article status
+export const putArticleStatus = async (status, id) => {
+  try {
+    await fetch(`${API}/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(status),
+    });
   } catch (error) {
     console.log(error);
   }
